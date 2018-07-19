5ec86b95ff9f312e7132e5d98937eb53bdd6efb2
Wiktoria Abramczyk <wiktoriaabramczyk17@gmail.com>
2018-07-19 19:48:31 +0200

    gra wieza - usprawnienia techniczne

--- npm_react/gra-wieza/src/App.js
+++ npm_react/gra-wieza/src/App.js
@@ -4,11 +4,44 @@ import './App.css';
 class App extends Component {
   constructor(){
     super()
+    let pudelkoPrzyciskow = []
+    for(let i = 0; i <30; i++){ 
+      pudelkoPrzyciskow.push({
+        id: i,
+        czyAktywny: false,
+        tekstPrzycisku: '*'
+      })
+    }
     this.state= {
       czyGraRozpoczeta : 0,
-      poziomAktywny : 3,
+      poziomAktywny : 0,
       czyWygrana: 0,
+      listaPrzyciskow: pudelkoPrzyciskow,
+      maxPoziom: 11
+      
     }
+  }
+  wyswietlListePrzycisow = () => {
+    return(
+      <div style = {{'width' : 64}}>
+        {this.state.listaPrzyciskow.map((x, index)=>  {
+          let styleDisplay = {'display' : 'inline-block'}
+          if(index === 2 || index === 6 || index === 9){
+            styleDisplay = {
+              'display' : 'inline-block'
+            }
+          }
+          return( 
+            <div style = {styleDisplay} key = {x.id}>
+              <button 
+                disabled = {!x.czyAktywny}
+                onClick = {() => this.nastepnyPoziom(index)}
+              >{x.tekstPrzycisku}</button>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
   wyswietlKomunikatWygrana = () => {
     if(this.state.czyWygrana === 1){
@@ -22,7 +55,7 @@ class App extends Component {
       );
     }
   }
-  przyciski = () => {
+  wyswietlPrzyciski = () => {
     return(
       <div>
         <div 
@@ -84,38 +117,80 @@ class App extends Component {
     }
   }
   rozpocznijGre = () => {
+    let nowaListaPrzyciskow = this.state.listaPrzyciskow.slice()
+    nowaListaPrzyciskow.forEach((item, index) =>{
+      if(index < 3){
+        nowaListaPrzyciskow[index].czyAktywny=true
+      }
+    })
+    nowaListaPrzyciskow.forEach((item, index) => {
+      nowaListaPrzyciskow[index].tekstPrzycisku = '*'
+    })
     this.setState({
       czyGraRozpoczeta : 1,
       poziomAktywny : 1,
-      czyWygrana : 0
-    })
+      czyWygrana : 0,
+      listaPrzyciskow : nowaListaPrzyciskow
+    }) 
   }
   zakonczGre = () => {
+    let nowaListaPrzyciskow = this.state.listaPrzyciskow.slice()
+    nowaListaPrzyciskow.forEach((item, index) => {
+      nowaListaPrzyciskow[index].tekstPrzycisku = '*'
+    })
     this.setState({
       czyGraRozpoczeta: 0,
       poziomAktywny: 0,
       czyWygrana : 0
     })
   }
-  nastepnyPoziom = (event) => {
-    let nowyStan = this.state.poziomAktywny + 1
-    if(nowyStan === 5){
-      this.setState({
-        czyWygrana: 2
+  nastepnyPoziom = (kliknietyIndex) => {
+    console.log(kliknietyIndex)
+    let czyPrzegrana = Math.random() < 0.33
+    if(czyPrzegrana === true){
+      let nowaListaPrzyciskow = this.state.listaPrzyciskow.slice()
+      nowaListaPrzyciskow.forEach((item, index) => {
+        nowaListaPrzyciskow[index].czyAktywny = false;
+        nowaListaPrzyciskow[index].tekstPrzycisku = '*'
+      })
+      nowaListaPrzyciskow[kliknietyIndex].tekstPrzycisku = '-'
+      this.setState ({
+        czyWygrana : 1,
+        listaPrzyciskow : nowaListaPrzyciskow
       })
     }else{
-      this.setState({
-      poziomAktywny : nowyStan
+      let nowyStan = this.state.poziomAktywny + 1
+      let nowaListaPrzyciskow = this.state.listaPrzyciskow.slice()
+      nowaListaPrzyciskow[kliknietyIndex].tekstPrzycisku = '`'
+      nowaListaPrzyciskow.forEach((item, index) => {
+        let poczatek = 3*(nowyStan - 1)
+        let koniec = poczatek + 2
+        if(index >= poczatek && index <= koniec){
+          nowaListaPrzyciskow[index].czyAktywny=true
+        }else{
+          nowaListaPrzyciskow[index].czyAktywny=false
+        }
       })
+      if(nowyStan === this.state.maxPoziom){
+        this.setState({
+          czyWygrana: 2
+        })
+      }else{
+        this.setState({
+        poziomAktywny : nowyStan,
+        listaPrzyciskow: nowaListaPrzyciskow
+        })
+      }
     }
   }
   render(){
     return(
       <div>
-        {this.przyciski()}
+        {/* {this.wyswietlPrzyciski()} */}
         {this.wyswietlPrzyciskStart()}
         {this.wyswietlPrzyciskKoniec()}
         {this.wyswietlKomunikatWygrana()}
+        {this.wyswietlListePrzycisow()}
       </div>
     );
   }
