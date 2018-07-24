c22e9cb028f1761ee332c996bdfe99eb14a7c3b7
Wiktoria Abramczyk <wiktoriaabramczyk17@gmail.com>
2018-07-24 22:09:35 +0200

    kuuurss

--- npm_react/kurs-es6/src/App.test.js
+++ npm_react/kurs-es6/src/App.test.js
@@ -5,13 +5,58 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme'
 import { copyFile } from 'fs';
 
-describe('kurs z lista', () => {
-  // it('kurs es6', () => {
-
+describe('kurs es6', () => {
+  it('napisanie funkcji uzywajac wyrazu function', () => {
+    let createGreeting = function(message, name){
+      return message + name
+    }
+    createGreeting()
+  })
+  it('napisanie funkcji z uzyciem strzalek', () =>{
+    let arrowGreeting = (message, name) => message + name;
+    arrowGreeting()
+  })
+  it('napisanie funkcji z uzyciem strzalek i jednego parametru', () =>{
+    let arrowGreeting = message => 'hello';
+    arrowGreeting()
+  })
+  it('x do kwadratu', () => {
+    let squared = x => x*x;
+    squared()
+  })
+  
+  // it('test pomocniczy', () =>{
+  //   let deliveryBoy = {
+  //     name: 'John',
+  //     handleMessage: function (message, handler){
+  //       handler(message);
+  //     },
+  //     receive: function () {
+  //       this.handleMessage('Hello, ', message => console.log(message + this.name))
+  //     }
+  //   }
+  //   deliveryBoy.receive()
   // })
-  it('pierwszy test', () => {
-      let zmienna = 10;
-      expect(zmienna).to.equal(10)
-    });
-
+  
+  // it('funkcja pomocnicza', () => {
+  //   let fs = [];
+  //   for(let i = 0; i < 10; i++){
+  //     fs.push(function () {
+  //       console.log(i);
+  //     })
+  //   }
+  //   fs.forEach(function (f) {
+  //     f();
+  //   })
+  // })
+  // it('przekazanie propsow', () =>{
+  //   function greet(greeting, name = 'John'){
+  //     console.log(greeting + ", " + name);
+  //   }
+  //   greet('Hello', 'Bill');
+  // })
+  // it('pisanie funkcji w jednej linii', () =>{
+  //   let receive = (complete = () => console.log('complete')) => complete()
+  //   receive();
+  // })
 })
