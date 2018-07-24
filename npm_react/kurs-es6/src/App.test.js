2ea7b970f7ceb6a759b03626881bb5167cf2c49b
Wiktoria Abramczyk <wiktoriaabramczyk17@gmail.com>
2018-07-24 16:52:46 +0200

    utworzenie testów do kursu

---
+++ npm_react/kurs-es6/src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { expect } from 'chai';
+import { shallow } from 'enzyme'
+import { copyFile } from 'fs';
+
+describe('kurs z lista', () => {
+  // it('kurs es6', () => {
+
+  // })
+  it('pierwszy test', () => {
+      let zmienna = 10;
+      expect(zmienna).to.equal(10)
+    });
+
+})
