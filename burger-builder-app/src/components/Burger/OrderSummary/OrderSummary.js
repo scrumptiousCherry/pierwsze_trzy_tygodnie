3ddb6572e182e3599d789c25279d7f725676a4b0
Wiktoria Abramczyk <wiktoriaabramczyk17@gmail.com>
2018-08-08 15:21:17 +0200

    usunięcie auksa :)

--- burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, { Component }  from 'react';
-import Aux from '../../../hoc/Auxx/Aux';
+import Aux from '../../../hoc/Auks/Auks';
 import Button from '../../UI/Button/Button';
 
 
--- burger-builder-app/src/components/UI/Modal/Modal.js
+++ burger-builder-app/src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import classes from './Modal.css';
-import Aux from '../../../hoc/Auxx/Aux';
+import Aux from '../../../hoc/Auks/Auks';
 import Backdrop from '../Backdrop/Backdrop'
 
 
